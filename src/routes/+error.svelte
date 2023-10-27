d116ddc287d7ba5238f558ae24b8b141d93d4752
Jonas <publik.froeller.jonas@gmx.at>
2023-10-27 21:33:34 +0200

    feat(cfg): added +error.svelte, updated app.html and configurations

---
+++ src/routes/+error.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import { buttonVariants } from '$lib/components/ui/button';
+</script>
+
+<section class="flex flex-col items-center justify-center h-screen text-center">
+	<h1 class="text-8xl">{$page.status} {$page?.error?.message}</h1>
+	<p class="mb-4 text-4xl text-primary">Path: {$page.url.pathname}</p>
+	<a href="/" class={buttonVariants({ variant: 'outline' })}> Home </a>
+</section>
