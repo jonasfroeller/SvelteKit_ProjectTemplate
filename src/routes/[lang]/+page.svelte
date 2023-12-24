32901f3c28d0969d3dd210e2d7d0488765c181e0
Jonas <publik.froeller.jonas@gmx.at>
2023-12-24 14:32:47 +0100

    feat(title): added greeting to svelte:head

--- src/routes/[lang]/+page.svelte
+++ src/routes/[lang]/+page.svelte
@@ -3,6 +3,10 @@
 	import Icon from '@iconify/svelte';
 </script>
 
+<svelte:head>
+	<title>{$LL.greeting()}</title>
+</svelte:head>
+
 <h1 class="flex items-end font-bold text-8xl">
 	{$LL.greeting()}&nbsp;<Icon icon="bx:world" />!
 </h1>
