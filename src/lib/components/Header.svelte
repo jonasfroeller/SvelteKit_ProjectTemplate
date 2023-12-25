b19cead0e0ac4e419f41c67fc2d21906d7d6c722
Jonas <publik.froeller.jonas@gmx.at>
2023-12-25 01:42:40 +0100

    feat(Header.svelte): added mode-watcher

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,7 +1,15 @@
 <script lang="ts">
 	import LocaleSwitcher from '$lib/components/LocaleSwitcher.svelte';
+	import { Button } from './ui/button';
+	import { toggleMode } from "mode-watcher";
+	import Icon from '@iconify/svelte';
 </script>
 
-<header class="fixed top-0 w-full p-4 bg-primary text-primary-foreground">
+<header class="fixed top-0 flex justify-between w-full p-4">
 	<LocaleSwitcher />
+	<Button on:click={toggleMode} variant="outline" size="icon">
+		<Icon icon="bx:sun" class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+		<Icon icon="bx:moon" class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+		<span class="sr-only">Toggle theme</span>
+	</Button>
 </header>
--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -5,6 +5,6 @@
 
 <section class="flex flex-col items-center justify-center h-screen text-center">
 	<h1 class="text-8xl">{$page.status} {$page?.error?.message}</h1>
-	<p class="mb-4 text-4xl text-primary">Path: {$page.url.pathname}</p>
+	<p class="mb-4 text-4xl">Path: {$page.url.pathname}</p>
 	<a href="/" class={buttonVariants({ variant: 'outline' })}> Home </a>
 </section>
